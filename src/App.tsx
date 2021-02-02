682e5873dbb93d0186d2989d5a3681655c666bf2
Steve Min <stevekmin@gmail.com>
2021-02-02 07:17:33 -0800

    update get total items function

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const App = () => {
 
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
 
-  const getTotalItems = (items: CartItemType[]) => null;
+  const getTotalItems = (items: CartItemType[]) => items.reduce((acc: number, item) => acc + item.amount, 0);
   const handleAddToCart = (clickedItem: CartItemType) => null;
   const handleRemoveFromCart = () => null;
 
