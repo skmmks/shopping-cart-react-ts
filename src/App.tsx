56c4734c3e2a4304b67703612c0ec3b8c0c99539
Steve Min <stevekmin@gmail.com>
2021-02-05 07:42:35 -0800

    setup handleadd to cart function

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,15 @@ const App = () => {
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
 
   const getTotalItems = (items: CartItemType[]) => items.reduce((acc: number, item) => acc + item.amount, 0);
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+
+      if (isItemInCart) {
+        return prev.map((item) => (item.id === clickedItem.id ? { ...item, amount: item.amount + 1 } : item));
+      }
+    });
+  };
   const handleRemoveFromCart = () => null;
 
   if (isLoading) return <LinearProgress />;
--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -28,7 +28,7 @@ const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
         </Button>
       </div>
     </div>
-    ;
+    <img src={item.image} alt={item.title} />
   </Wrapper>
 );
 
