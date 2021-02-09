989f17d1146f53a11c844937d5b50d11611de91d
Steve Min <stevekmin@gmail.com>
2021-02-09 07:11:07 -0800

    add remove from cart function

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,18 @@ const App = () => {
       return [...prev, { ...clickedItem, amount: 1 }];
     });
   };
-  const handleRemoveFromCart = () => null;
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((acc, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return acc;
+          return [...acc, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...acc, item];
+        }
+      }, [] as CartItemType[])
+    );
+  };
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>There is an error...</div>;
